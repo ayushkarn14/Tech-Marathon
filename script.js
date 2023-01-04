53acfa577682e7a9382d055488a648fc31be1e69
ayushkarn14 <ayushkarn14@gmail.com>
2023-01-04 18:50:13 +0530

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+var deadline = new Date("Jan 5, 2023 00:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.getElementById("d").innerHTML = days + " days";
+    document.getElementById("h").innerHTML = hours + " hours";
+    document.getElementById("m").innerHTML = minutes + " mins";
+    document.getElementById("s").innerHTML = seconds + " secs";
+    // document.getElementById("demo").innerHTML =
+    //     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+}, 1000);
