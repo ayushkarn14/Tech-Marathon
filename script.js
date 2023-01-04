331b5db28c690623329b088b6a616c4562b94d1b
ayushkarn14 <ayushkarn14@gmail.com>
2023-01-04 18:55:21 +0530

    second

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var deadline = new Date("Jan 5, 2023 00:00:00").getTime();
+var deadline = new Date("Jan 10, 2023 00:00:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var t = deadline - now;
